2a2fe255e1961fe3071a7fda47389ce1f54549e7
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2021-01-02 09:40:02 -0600

    Setup homepage

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,15 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'jakebmx',
+    title: 'JAKEBMX',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap' }
     ]
   },
 
